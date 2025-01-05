0a8d62616fae70089cb58bf6a17a9c711ef5070c
Muthana-Ot <muthanadandashi54@gmail.com>
2025-01-05 15:11:53 +0200

    `Removed all PHP files from TaskManager directory`

--- TaskManager/connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn=mysqli_connect("fdb1029.awardspace.net","4571559_taskmanager", "muthana987","4571559_taskmanager");
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
